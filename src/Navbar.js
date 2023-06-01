31e616a721b23822f966cc7c888848bb9c42ba07
Shaktidhar <sdmohapatra7@gmail.com>
2023-06-01 21:40:03 +0530

    adding styled Components

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,20 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Nav = styled.div`
+    height:50px;
+    background:linear-gradient(170deg,#1bc059,#0d47a1);
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+    position:relative;
+`;
+const Title = styled.div`
+    font-size:30px;
+    color:#fff;
+    font-weight:600;
+    font-family:'Times New Roman',Times,serif;
+    text-transform:uppercase;
+    margin-left:20px;
+    &:hover{color:cyan}
+`;
+const CartImg = styled.img`
+    height:40px;
+    margin-right:20px;
+`;
+const CartIconContainer = styled.div`
+    position:relative;
+    cursor:pointer;
+`;
+const CartCount = styled.div`
+    background-color: ${(props)=>props.color};
+    border-radius:50%;
+    padding:4px 8px;
+    position:absolute;
+    right:10px;
+    top:-5px;
+    font-size:12px;
+    visibility: ${(props)=>props.show?'visible':'hidden'}
+`;
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div style={styles.nav}> 
-                <div style={styles.title}>Movie-App</div>
-                <div style={styles.cartContainer}>
-                    <img alt='cart-icon' src='https://cdn-icons-png.flaticon.com/128/9453/9453946.png' style={styles.cartIcon}/>
-                    <span style={styles.cartCount}>0</span>
-                </div>
-            </div>
+            <Nav> 
+                <Title>Movie-App</Title>
+                <CartIconContainer>
+                    <CartImg alt='cart-icon' src='https://cdn-icons-png.flaticon.com/128/9453/9453946.png' />
+                    <CartCount color='yellow' show={true}>6</CartCount>
+                </CartIconContainer>
+            </Nav>
         )
     }
 }
 
-const styles = {
+/*const styles = {
     nav:{
         width:'100%',
         height:50,
@@ -49,4 +86,4 @@ const styles = {
         top:-5,
         fontSize:12
     }
-}
+}*/
